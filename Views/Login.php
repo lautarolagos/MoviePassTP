20126f42571a4655156fd1f4d893bc97a720864f
LeandroNP98 <leandro.nahuel.pintos@gmail.com>
2020-10-09 07:48:01 -0300

    Css y fondo

--- Views/Login.php
+++ Views/Login.php
@@ -2,26 +2,40 @@
      namespace Views;
      include('Header.php');
   ?>
-<html>
-     <main class="">
-          <div class="">
-               <header class="">
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <style>
+            body{
+               background-image: url(Img/MoviePass.jpg);
+               background-repeat: no-repeat;
+               background-size: 1920px 1080px;
+               }
+       </style>
+       <title>Movie Pass®</title>
+  </head>
+  <body>
+  <main >
+          <div class="cont-form">
+               <header >
                     <h2>Log In</h2>
                </header>
-               <form action="<?php echo FRONT_ROOT ?>/Login/Check" method="POST"  class="">
-                    <div class="">
-                         <label for="email">Email</label>
-                         <input type="text" name="email" class="" placeholder="Email">
+               <form action="<?php echo FRONT_ROOT ?>/Login/Check" method="POST"  >
+                    <div >
+                         <label class ="align" for="email">Email</label>
+                         <input type="text" name="email"  placeholder="Email">
                     </div>
-                    <div class="">
+                    <div >
                          <label for="password">Password</label>
-                         <input type="password" name="password" class="" placeholder="Password">
+                         <input type="password" name="password"  placeholder="Password">
                     </div>
-                    <button class="" type="submit">Log In</button>
+                    <button  type="submit">Log In</button>
                </form>
                <!-- lo de abajo no anda bien, cuidado -->
-               <form action="<?php echo VIEWS_PATH ?>Register.php" method="POST" class="">
-               <button class="" type="submit">Register</button>
+               <form action="<?php echo VIEWS_PATH ?>Register.php" method="POST" >
+               <button  type="submit">Register</button>
                </form>
           </div>
      </main>
@@ -30,4 +44,5 @@
                          echo $message;
                     }
                     ?>
-</html>
+  </body>
+  </html>
