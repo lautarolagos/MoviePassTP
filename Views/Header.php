1cd157c54f70b8dd80c028dafa4d9c57a90294a7
LeandroNP98 <leandro.nahuel.pintos@gmail.com>
2020-10-10 08:25:44 -0300

    Interface de css(por alguna razon, solo anda en el login)

--- Views/Header.php
+++ Views/Header.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign in - MoviePass</title>
-    <link rel="stylesheet" href="Views/Css/LoginStyle.css" type="text/css">
+    <link rel="stylesheet" href="Views/Css/Style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
 </head>
 <body>
 </body>
--- Views/Login.php
+++ Views/Login.php
@@ -12,33 +12,37 @@
                background-image: url(Img/MoviePass.jpg);
                background-repeat: no-repeat;
                background-size: 1920px 1080px;
+               -webkit-background-size: cover;
+               background-size: cover;
+               background-attachment: fixed;
                }
        </style>
        <title>Movie Pass®</title>
   </head>
   <body>
-  <main >
+       <div>
+            <h1>M<span>OVIE</span> P<span>ASS</span></h1>
+       </div>
           <div class="cont-form">
-               <header >
-                    <h2>Log In</h2>
-               </header>
-               <form action="<?php echo FRONT_ROOT ?>/Login/Check" method="POST"  >
-                    <div >
-                         <label class ="align" for="email">Email</label>
-                         <input type="text" name="email"  placeholder="Email">
+               <form class="form" action="<?php echo FRONT_ROOT ?>/Login/Check" method="POST">
+                    <div>
+                         <h2 class="form-title">Log In</h2>
                     </div>
                     <div >
-                         <label for="password">Password</label>
-                         <input type="password" name="password"  placeholder="Password">
+                         <label class ="form-label" for="email">Email</label>
+                         <input class="form-input"  type="text" name="email"  placeholder="Email">
                     </div>
-                    <button  type="submit">Log In</button>
+                    <div >
+                         <label class ="form-label" for="password">Password</label>
+                         <input class="form-input" type="password" name="password"  placeholder="Password">
+                    </div>
+                    <button class="btn-submit" type="submit">Log In</button>
                </form>
                <!-- lo de abajo no anda bien, cuidado -->
-               <form action="<?php echo VIEWS_PATH ?>Register.php" method="POST" >
-               <button  type="submit">Register</button>
+               <form class="form" action="<?php echo VIEWS_PATH ?>Register.php" method="POST">
+               <button  class="btn-submit" type="submit">Register</button>
                </form>
           </div>
-     </main>
      <?php if(isset($message))
                     {
                          echo $message;
