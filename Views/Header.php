b4a4fc8ffe25af7f8be3c8a3eb7d46c8d855a837
lautarolagos <lautarolagosleiva@hotmail.com>
2020-10-10 16:21:01 -0300

    vistas

--- Views/Header.php
+++ Views/Header.php
@@ -4,18 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign in - MoviePass</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="Views/Css/Style.css" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
 </head>
 <body>
-=======
-    <link rel="stylesheet" href="" type="">
-</head>
-<body>
-    
-
->>>>>>> ab00ea37df027eca6eff9dd093dd07eeea074050
 </body>
 </html>
