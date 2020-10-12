67f2a84670df1bde906b77f55d03052d289df2cd
LeandroNP98 <leandro.nahuel.pintos@gmail.com>
2020-10-12 02:54:24 -0300

    Nav & config definitive

--- Views/Nav.php
+++ Views/Nav.php
@@ -1,20 +1,31 @@
 <?php
     namespace Views;
 ?>
-<nav>
-    <span>
-        <!-- comente lo de abajo por un warning que tira. Hay que arreglarlo -->
-        <!-- Hola <strong><?php session_start(); echo $_SESSION['email']?></strong> -->
-    </span>
-    <ul>
-        <li>
-            <a href="<?php echo FRONT_ROOT ?>/Billboard">Billboard</a>
-        </li>
-        <li>
-            <a href="<?php echo FRONT_ROOT ?>/Logout">Logout</a>
-        </li>
-        <li>
-            <a class="nav-link" href="<?php echo FRONT_ROOT ?>/Cinema/ShowCinemaList">Cinema Listings</a>
-        </li>
-    </ul>
-</nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body class="nav-link">
+    <div id="menu">
+        <div>
+            <ul>
+                <li>
+                    <a class="item-r"><?php session_start(); echo $_SESSION['email']?></a>
+                </li>
+
+                <li>
+                    <a class="item-r" href="<?php echo FRONT_ROOT ?>/Billboard">Billboard</a>
+                </li>
+                <li>
+                    <a class="item-r" href="<?php echo FRONT_ROOT ?>/Logout">Logout</a>
+                </li>
+                <li>
+                    <a  class="item-r" href="<?php echo FRONT_ROOT ?>/Cinema/ShowCinemaList">Cinema Listings</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
