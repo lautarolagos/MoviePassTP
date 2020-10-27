54b37ac4d7098e43bc637490153652dae5d47a12
LeandroNP98 <leandro.nahuel.pintos@gmail.com>
2020-10-27 16:15:40 -0300

    Nuevo NAV (falta icono responsive)

--- Views/Nav.php
+++ Views/Nav.php
@@ -1,5 +1,6 @@
 <?php
     namespace Views;
+    include_once('Header.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,31 +8,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body >
-    <div id="menu">
-    <ul>
-        <li class="item-l">
-                <a>Hola <strong><?php echo $_SESSION['email']?></strong></a> 
-            </li>
-            <li>
-                <a  href="#">Billboard</a>
-            </li>
-            <?php if($_SESSION['isAdmin']=="1")
-            {
-            ?>
-            <li>
-                <a   href="<?php echo FRONT_ROOT ?>Cinema/ShowAddView">Add Cinema</a>
-            </li>
-            <?php 
-            }
-            ?>
-            <li>
-                <a   href="<?php echo FRONT_ROOT ?>Cinema/ShowCinemaList">Cinema Listings</a>
-            </li>
-            <li>
-                <a  href="<?php echo FRONT_ROOT ?>Session/Logout">Logout</a>
-            </li>
-        </ul>
-    </div>
+<body id="menu">
+    <header>
+        <input type="checkbox" id="btn-menu">
+        <label for="btn-menu"> <img src="boton-nav.png" alt=""></label>
+            <nav class="menu">
+                <ul>
+                    <li>
+                        <a>Welcome <strong><?php echo $_SESSION['email']?></strong></a>
+                    </li>
+                    <li>
+                        <a href="#">Billboard</a>
+                    </li>
+                    <?php if($_SESSION['isAdmin']=="1")
+                    {
+                    ?>
+                    <li>
+                        <a href="<?php echo FRONT_ROOT ?>Cinema/ShowAddView">Add Cinema</a>
+                    </li>
+                    <?php 
+                    }
+                    ?>
+                    <li>
+                        <a href="<?php echo FRONT_ROOT ?>Cinema/ShowCinemaList">Cinema Listings</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo FRONT_ROOT ?>Session/Logout">Logout</a>
+                    </li>
+                    
+                </ul>
+            </nav>
+    </header>
 </body>
 </html>
