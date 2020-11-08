6cd911f1a4db06a4a5be1d4541564adfedae2d92
lautarolagos <lautarolagosleiva@hotmail.com>
2020-11-08 00:47:09 -0300

    creacion modelo Purchase and other stuff

--- Models/Ticket.php
+++ Models/Ticket.php
@@ -4,34 +4,45 @@
     //number,   QRcode 
     class Ticket
     {
-        private $number;
+        private $number; // seria como el ID
         private $QRcode;
-        // Creo que habria que agregar una variable que sea Fecha de la funcion
+        private $purchase; // la compra a la que pertenece
 
-        function __construct($number = NULL, $QRcode = NULL)
+        function __construct($number = NULL, $QRcode = NULL, $purchase = NULL)
         {
             $this->number = $number;
-            $this->QRcode = $QRcode; 
+            $this->QRcode = $QRcode;
+            $this->purchase = $purchase;
         }
 
+        // Getters
         public function getNumber()
         {
-            return $this->Number;
+            return $this->number;
         }
-
-        public function setNumber($number)
-        {
-            $this->number = $number;
-        }
-
         public function getQRCode()
         {
             return $this->QRcode;
         }
+        public function getPurchase()
+        {
+            return $this->purchase;
+        }
 
+        // Setters
+        public function setNumber($number)
+        {
+            $this->number = $number;
+        }
         public function setQRCode($QRcode)
         {
             $this->QRcode = $QRcode;
         }
+        public function setPurchase($purchase)
+        {
+            $this->purchase = $purchase;
+        }
+
+
     }
 ?>
