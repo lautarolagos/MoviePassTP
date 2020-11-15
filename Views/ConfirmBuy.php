ddc1dfbeae21c66f4776da58119c47a148a3c90d
lautarolagos <lautarolagosleiva@hotmail.com>
2020-11-15 15:59:18 -0300

    arreglados bugs de confirm buy

--- Views/ConfirmBuy.php
+++ Views/ConfirmBuy.php
@@ -40,26 +40,27 @@
                     <label class="form-label-buy" type="text" id="dateTime" name="dateTime" class="" placeholder=""><?php echo $projection->getDate(); echo " at " .$projection->getStartTime();?></label>
                 </div>
                 <div>
-                <label class="title-buy"for="totalPrice">Subtotal: <?php echo "$". $subtotal ?></label>
+                <label class="title-buy"for="subtotal">Subtotal:</label>
+                <label class="form-label-buy" for="subtotal" name="subtotal"><?php echo "$" .$subtotal; ?></label>
+                </div>
                 <div>
-                <?php if($discount!=0){
-                    ?>
-                    <label for="" style="padding-top:5px;">Discount applied! ($-<?php echo $discount; ?>)</label>
-                <?php }
-                ?> 
+                <label class="title-buy"for="discount">Discount:</label>
+                <label class="form-label-buy" for="discount" name="discount"><?php echo "$" .$discount; ?></label>
                 </div>
-                <label class="title-buy" for="totalPrice">Total Price:</label>
-                    <label class="form-label-buy" type="text" id="totalPrice" name="totalPrice" class="" placeholder="<?php /*echo "$ $totalPrice";*/?>"> </label>
+                <div>
+                <label class="title-buy"for="totalPrice">Total Price:</label>
+                <label class="form-label-buy" for="totalPrice" name="totalPrice"><?php echo "$" .$totalPrice; ?></label>
                 </div>
-                </form>
-                <form class="form-buy" action="<?php echo FRONT_ROOT ?>Purchase/ProcessBuy" method="POST">
-                <label type="hidden" name="quantity" value="<?php echo $quantity; ?>">
-                <label type="hidden" name="idProjection" value="<?php echo $projection->getIdProjection(); ?>">
-                <label type="hidden" name="subtotal" value="<?php echo $subtotal; ?>">
-                <label type="hidden" name="discount" value="<?php echo $discount; ?>">
-                <label type="hidden" name="totalPrice" value="<?php echo $totalPrice; ?>">
+            </form>   
+            <form class="form-buy" action="<?php echo FRONT_ROOT ?>Purchase/ProcessBuy" method="POST">
+                <input type="hidden" name="quantity" value="<?php echo $quantity; ?>">
+                <input type="hidden" name="idProjection" value="<?php echo $projection->getIdProjection(); ?>">
+                <input type="hidden" name="subtotal" value="<?php echo $subtotal; ?>">
+                <input type="hidden" name="discount" value="<?php echo $discount; ?>">
+                <input type="hidden" name="totalPrice" value="<?php echo $totalPrice; ?>">
                 <center><button class="btn-submit" type="submit">CONFIRM BUY</button></center>
-                </form>
+            </form>
         </div>
+    </div>    
     </body>
 </html>
